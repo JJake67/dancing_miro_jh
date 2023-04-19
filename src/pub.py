de2a0411863d3efbf9987ac45aa6e591214c5cfa
AlexandrLucas <alex.lucas@sheffield.ac.uk>
2023-04-19 10:40:00 +0100

    add yaw example

--- src/pub.py
+++ src/pub.py
@@ -24,7 +24,8 @@ class JointPublisher(object):
         self.cosmetic_pub = rospy.Publisher(
             self.topic_base_name + "/control/cosmetic_joints", Float32MultiArray, queue_size=0
         )
-        self.time_scale = 30 #for how fast the movement is 
+        self.time_scale = 5 #for how fast the movement is 
+        self.blink_freq = 2 #for how fast the movement is 
         self.kinematic_joint_cmd = JointState()
         self.kinematic_joint_cmd.position = [0, 0, 0, 0]
         self.cosmetic_joint_cmd = Float32MultiArray()   
@@ -36,6 +37,16 @@ class JointPublisher(object):
         self.kinematic_pub.publish(self.kinematic_joint_cmd)
 
     # movement for the tail, eye lid, ears
+    def set_move_cosmetic2(self):
+        self.cosmetic_joint_cmd = Float32MultiArray()  
+        self.data_calib = [0,0.5,0.5,0.5,0.33,0.33] 
+        self.cosmetic_joint_cmd.data = self.data_calib
+        #self.cosmetic_joint_cmd.data = [0,0,0,0,0,0]
+            
+        #self.cosmetic_joint_cmd.data = [tail_pitch,tail_yaw,left_eye,right_eye,left_ear,right_ear]
+        self.cosmetic_pub.publish(self.cosmetic_joint_cmd)
+
+    # movement for the tail, eye lid, ears
     def set_move_cosmetic(self, tail_pitch = 0, tail_yaw = 0, left_eye = 0, right_eye = 0, left_ear = 0, right_ear = 0):
         self.cosmetic_joint_cmd.data = [tail_pitch,tail_yaw,left_eye,right_eye,left_ear,right_ear]
         self.cosmetic_pub.publish(self.cosmetic_joint_cmd)
@@ -43,7 +54,7 @@ class JointPublisher(object):
     # a sample on how the miro can blink
     def blink_sample(self):
         current_time = time.time()
-        set_angle = np.sin(self.time_scale*(current_time - self.start))
+        set_angle = np.sin(self.blink_freq*(current_time - self.start))
         tail_pitch = self.cosmetic_joint_cmd.data[0]
         tail_yaw = self.cosmetic_joint_cmd.data[1]
         left_eye = set_angle
@@ -101,13 +112,48 @@ class JointPublisher(object):
         self.set_move_kinematic( tilt, lift, yaw, pitch)
         #rospy.sleep(0.05)
 
-movement = JointPublisher()
-while not rospy.is_shutdown():
-    #movement.blink_sample()
-    movement.ear_sample()
-    #movement.tail_sample()
-    movement.head_sample()
-    #movement.rotate_sample()
-    #rospy.sleep(0.5)
-    print(np.sin(movement.time_scale*(time.time() - movement.start))) 
+    def sine_generator(self, mx=1, mn=0, offset=0, freq=1, phase=0, t=1.0, t0=0):
+        return ((mx-mn) * np.sin (freq*(t-t0) + phase) / 2.0 + offset)
 
+    def yaw_movement(self, t, t0):
+        self.kinematic_joint_cmd = JointState()
+        yaw_freq = 3
+        yaw = self.sine_generator(55, -55, 0, yaw_freq, 0, t, t0)
+        print(yaw)
+        self.kinematic_joint_cmd.position = [0, 0, yaw, 0]
+        self.kinematic_pub.publish(self.kinematic_joint_cmd)
+
+    def pitch_movement(self, tilt = 0, lift = 0, yaw = 0, pitch = 0):
+        pass
+
+
+movement = JointPublisher()
+t0 = rospy.Time.now().to_sec()
+while not rospy.is_shutdown():
+    t = rospy.Time.now().to_sec()
+    movement.yaw_movement(t, t0)
+    rospy.sleep(0.05)
+    # movement.blink_sample()
+    # movement.ear_sample()
+    #movement.tail_sample()
+    #movement.head_sample()
+    #movement.rotate_sample()
+    #movement.set_move_cosmetic2()
+
+    #print(np.sin(movement.time_scale*(time.time() - movement.start))) 
+
+
+
+"""
+Sine generators for different joints:
+A*sin(w*t + phi)
+
+1. Yaw
+min = -55, max = 55,
+yaw_joint_pose = 55 * sin (self.yaw_freq*(current_time - self.start) + phase)
+
+2. Pitch
+min = -22, max = 8,
+pitch_joint_pose = 15 * sin (self.yaw_freq*(current_time - self.start) + phase) - 7
+
+"""
