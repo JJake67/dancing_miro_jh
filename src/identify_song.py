47f7136756de143ec17fa1f5fc907c192f33db2a
Jake Holden <jnholden95@gmail.com>
2024-02-27 13:39:51 +0000

    tryna work for python2

--- src/identify_song.py
+++ src/identify_song.py
@@ -4,14 +4,16 @@ from shazamio import Shazam
 
 print('Get current working directory : ', os.getcwd())
 directory = os.getcwd()
-async def main():
+def main():
   shazam = Shazam()
   # Waits until shazam identifies song
-  out = await shazam.recognize_song(directory+"\data\white_noise_5secs.mp3")
+  out = shazam.recognize_song(directory+"\data\white_noise_5secs.mp3")
   if len(out["matches"]) == 0 :
     print("soz")
   else:
     print(out["track"]["title"])
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(main())
+main = main()
+main.loop()
