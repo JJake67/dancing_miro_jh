9867a5a54f78a6fed605589c7682c896a8c9a475
Jake Holden <jnholden95@gmail.com>
2024-03-13 13:54:09 +0000

    e

--- audio_etraction/tempo.py
+++ audio_etraction/tempo.py
@@ -72,7 +72,7 @@ ax.legend(loc='upper right')
 avgFreq = sum(f0)/len(f0)
 #plt.show()
 """
-
+#Yellow
 #client_id = os.getenv("CLIENT_ID")
 client_id = "acbd6c4e089e4c9cb071ce9d3e4a9583"
 #client_secret = os.getenv("CLIENT_SECRET")
